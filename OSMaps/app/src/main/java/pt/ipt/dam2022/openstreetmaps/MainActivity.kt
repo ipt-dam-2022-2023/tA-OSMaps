94b7324b251d3944687142db82728784b349bf54
jcnpereira <casimiro@ipt.pt>
2022-12-15 16:18:48 +0000

    app access to map

--- OSMaps/app/src/main/java/pt/ipt/dam2022/openstreetmaps/MainActivity.kt
+++ OSMaps/app/src/main/java/pt/ipt/dam2022/openstreetmaps/MainActivity.kt
@@ -1,11 +1,90 @@
 package pt.ipt.dam2022.openstreetmaps
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import org.osmdroid.config.Configuration
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory
+import org.osmdroid.views.CustomZoomButtonsController
+import org.osmdroid.views.MapView
+import org.osmdroid.views.overlay.compass.CompassOverlay
 
 class MainActivity : AppCompatActivity() {
+
+    private val REQUEST_PERMISSIONS_REQUEST_CODE = 1
+    private lateinit var map:MapView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        requestPermissionsIfNecessary(
+                arrayOf(
+                        Manifest.permission.ACCESS_COARSE_LOCATION,
+                        Manifest.permission.ACCESS_FINE_LOCATION,
+                        Manifest.permission.ACCESS_WIFI_STATE,
+                        Manifest.permission.INTERNET,
+                        Manifest.permission.ACCESS_NETWORK_STATE,
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                        Manifest.permission.READ_EXTERNAL_STORAGE
+                )
+        )
+
+        // add the OpenStreetMap to activity
+        showMap()
     }
+
+    /**
+     * add the Open Street Map to the activity
+     */
+    private fun showMap() {
+        // defines only one Map in all program
+        Configuration.getInstance().setUserAgentValue(this.getPackageName())
+
+        map = findViewById(R.id.map)
+        map.setTileSource(TileSourceFactory.MAPNIK)
+        map.controller.zoomTo(17.0)
+        map.zoomController.setVisibility(CustomZoomButtonsController.Visibility.ALWAYS)
+        map.setMultiTouchControls(true) // para poder fazer zoom com os dedos
+
+        var compassOverlay = CompassOverlay(this, map)
+        compassOverlay.enableCompass()
+        map.overlays.add(compassOverlay)
+
+
+        Handler(Looper.getMainLooper()).postDelayed({
+          //  map.controller.setCenter(point)
+        }, 1000) // waits one second to center map
+    }
+
+
+    /**
+     * function to collect users' permissions
+     */
+    private fun requestPermissionsIfNecessary(permissions: Array<out String>) {
+        val permissionsToRequest = ArrayList<String>();
+        permissions.forEach { permission ->
+            if (ContextCompat.checkSelfPermission(
+                        this, permission
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                // Permission is not granted
+                permissionsToRequest.add(permission);
+            }
+        }
+        if (permissionsToRequest.size > 0) {
+            ActivityCompat.requestPermissions(
+                    this,
+                    permissionsToRequest.toArray(arrayOf<String>()),
+                    REQUEST_PERMISSIONS_REQUEST_CODE
+            );
+        }
+    }
+
+
 }
